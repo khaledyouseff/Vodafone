f06e748dd176e70afd46ad76033cb5aaa915085c
khaledyouseff <khaledyoussouph36@gmail.com>
2024-01-27 14:54:06 +0200

    adding class runner

---
+++ second-part-BDD/vodafone1/src/test/java/runners/runner.java
@@ -0,0 +1,7 @@
+package runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/features" , glue = "steps")
+public class runner extends AbstractTestNGCucumberTests {
+}
